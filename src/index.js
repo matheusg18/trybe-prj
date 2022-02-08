f082c71026448cde10d90821ae8f96d5ea9ef568
matheusg18 <matheusgabriel.18@gmail.com>
2022-02-08 16:49:17 -0300

    Commit inicial

---
+++ src/index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const readline = require('readline-sync');
+const { execSync } = require('child_process');
+const open = require('open');
+
+const name = readline.question('Digite seu nome e sobrenome?');
+const link = readline.question('Qual o link SSH do projeto?');
+const prefix = 'git@github.com:tryber/';
+const sufix = '.git';
+const repoName = link.replace(prefix, '').replace(sufix, '');
+const rgx = /(sd-[0-9]{3})(-[a-z]-)/i;
+const projectName = repoName.replace(rgx, '');
+const branchName = `${name.toLowerCase().replace(' ', '-')}-${projectName}`;
+
+execSync(`git clone ${link}`, { stdio: 'inherit' });
+execSync(
+  `cd ${repoName} && npm install && git checkout -b ${branchName} && git add . && git commit -m "commit inicial" -m "teste do script" && git push -u origin ${branchName}`,
+  { stdio: 'inherit' }
+);
+
+const prLink = `https://github.com/tryber/${repoName}/pull/new/${branchName}`;
+open(prLink);
